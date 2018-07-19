c833a4a230f81cdfa4803fadaae07a8d8c53afa8
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-07-18 22:52:11 -1000

    Finished #14

--- js/app.js
+++ js/app.js
@@ -2,31 +2,107 @@
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
 
+matCount.innerHTML = 11;
+
+
+
+
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
+
+msgCount.innerHTML = 23;
+
+
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
+fullname.innerHTML = 'Ronald McDonald';
+
+
+
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
+
+age.innerHTML = '63 Years Old';
+
 
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
 
+var jobBox = document.createElement('div');
+jobBox.id = 'job';
+jobBox.innerHTML = 'Clown and Restauranteur';
+data.appendChild(jobBox);
+
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
+
+var hobbyBox = document.createElement('div');
+hobbyBox.id = 'hobby';
+hobbyBox.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.';
+data.appendChild(hobbyBox);
+
+
+
 
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
 
+var locationBox = document.createElement('div');
+locationBox.id = 'location';
+locationBox.innerHTML = 'Honolulu, HI';
+data.appendChild(locationBox);
+
+
+
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
+
+var wantsBox = document.createElement('div');
+wantsBox.id = 'wants';
+wantsBox.innerHTML = 'Looking for a Mrs. McDonald';
+data.appendChild(wantsBox); 
+
+
+
+
 
 //9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
 
+var profileBox = document.createElement('p');
+profileBox.id = 'pro2'
+profileBox.innerHTML = 'A clown with a deep purpose of life of making people laugh and not cry.'
+profile.appendChild(profileBox);
+
+
+
+
+
 //10. In the div id matches section, target the first div element with the class name of firstName and change Name to Wendy.
+
+var nameElem = document.getElementsByClassName('firstName');
+nameElem[0].innerHTML = 'Wendy';
+
+
 
 //11. In the div id matches section, target the first div element with the class name of otherAge and change Age to 48.
 
+var ageElem = document.getElementsByClassName('otherAge');
+ageElem[0].innerHTML = '50';
+
+
+
+
 //12. In the div id matches section, target the first div element with the class name of status and change Status to Single Mother.
+
+
+
 
 //13. In the div id matches section, target the second div element with the class name of firstName and change Name to Peko Chan.
 
+var name2Elem = document.getElementsByClassName('firstName');
+name2Elem[1].innerHTML = 'Peko Chan';
+
+
+
 //14. In the div id matches section, target the second div element with the class name of otherAge and change Age to 68.
+
+var name2Age = document.getElementsByClassName('otherAge');
+name2Age[1].innerHTML = '68'
 
  //Final Boss Create your own profile into the page:
  //change image to match your profile
@@ -35,6 +111,9 @@
  //div with class name of status
  //div with class anem of Motto
 
+
+
+ 
 
 
 
